876158733eef3312f905b0855293cd087a5ec914
Gabriel Rebello <gabriel@meier.gta.ufrj.br>
2016-10-11 20:00:13 -0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+	#include <fstream>
 #include <vector>
 #include <cstdio>
 
@@ -19,16 +20,20 @@ struct AdjList {
 // Size of array will be V (number of vertices in graph)
 struct Graph {
     int m_V;
+    int m_E;
+    bool m_type;
     vector<AdjList> m_adjArray;
     vector<vector<bool> > m_adjMatrix;
     Graph(int V, bool type);
     AdjListNode* newNode(int dest);
     void addEdge(int src, int dest);
+    float degreeMean();
 };
  
 Graph::Graph(int V, bool type) {
     m_V = V;
-    
+    m_E = 0;
+    m_type = type;
     if(type == 0) {
         for(int i=0;i<V;i++) {
             AdjList newList;
@@ -50,14 +55,26 @@ AdjListNode* Graph::newNode (int dest) {
 }
 
 void Graph::addEdge(int src, int dest) {
-    AdjListNode* newNode = this->newNode(dest);
-    newNode->next = this->m_adjArray[src].head;
-    this->m_adjArray[src].head = newNode;
- 
-    // Since graph is undirected, add an edge from dest to src also
-    newNode = this->newNode(src);
-    newNode->next = this->m_adjArray[dest].head;
-    this->m_adjArray[dest].head = newNode;
+	m_E++;
+	if(m_type==0) {
+		AdjListNode* newNode = this->newNode(dest);
+		newNode->next = m_adjArray[src].head;
+		m_adjArray[src].head = newNode;
+
+		// Since graph is undirected, add an edge from dest to src also
+		newNode = this->newNode(src);
+		newNode->next = m_adjArray[dest].head;
+		m_adjArray[dest].head = newNode;
+	} else {
+		m_adjMatrix[src][dest]=1;
+
+		// Since graph is undirected, add an edge from dest to src also
+		m_adjMatrix[dest][src]=1;	
+	}
+}
+
+float Graph::degreeMean() {
+     return float(2*m_E/m_V);
 }
 
 
@@ -81,19 +98,30 @@ void printGraph(Graph graph)
 // Driver program to test above functions
 int main()
 {
-    // create the graph given in above fugure
-    bool type = 1;
-    int V = 5;
-    Graph graph(V, type);
-    cout << graph.m_V << endl;
-    //cout << graph.m_adjArray[2].head << endl;
-    //graph.addEdge(0, 1);
-    //graph.addEdge(0, 3);
-    //graph.addEdge(1, 4);
-    //graph.addEdge(2, 0);
 
+    ifstream data;
+    data.open("data.txt");
+
+    // Define data structure type: 0-Adjency list; 1-Adjency Matrix
+    bool type = 1;
+
+    // Read V from file and create new graph with V vertexes and 0 edges
+    int V;
+    data >> V;
+    Graph graph(V, type);
+
+    cout << "Number of vertexes: " <<  graph.m_V << endl;
+    //cout << graph.m_adjArray[2].head << endl;
+
+    //Dynamically create edges
+    int v1,v2;
+    while (data >> v1 >> v2)
+        graph.addEdge(v1-1,v2-1);
+
+    cout << "Number of edges: " << graph.m_E << endl;
+    cout << "Degree mean: " << graph.degreeMean() << endl;
     //cout << graph.m_adjArray[4].head << endl;
     //printGraph(graph);
-    cout << graph.m_adjMatrix[0][0] << endl;
+    cout << graph.m_adjMatrix[1][0] << endl;
     return 0;
 }
