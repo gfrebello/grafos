8ca83dfe6046158905093ed598e1c4566491f489
ducast <eduardo-castanho@hotmail.com>
2016-11-16 15:29:26 -0200

    Add files via upload
    main atualizada com distancia média

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -8,11 +8,13 @@
 #include <ctime>
 #include <map>
 #include <unistd.h>
+#include <typeinfo>
+#include <bits/stdc++.h>
 
 #include "binaryheap.hpp"
 
 using namespace std;
- 
+
 bool less_vectors(const vector<int>& a, const vector<int>& b) {
 	return a.size() > b.size();
 }
@@ -24,12 +26,12 @@ struct AdjListNode {
     struct AdjListNode* next;
 
 };
- 
+
 // A structure to represent an adjacency list
 struct AdjList {
     struct AdjListNode *head;  // pointer to head node of list
 };
- 
+
 // A structure to represent a graph. A graph is an array of adjacency lists.
 // Size of array will be V (number of vertices in graph)
 struct Graph {
@@ -44,16 +46,19 @@ struct Graph {
     AdjListNode* newNode(int dest, float weight);
     void addEdge(int src, int dest);
     void addEdge(int src, int dest, float weight);
-    vector<int> BFS(int root);
+    float BFS(int root);
+    int Kruskal();
+    vector<float> dijkstraDists;
     vector<bool> DFS(int root);
     vector<bool> Dijkstra(int root);
     vector<int> MinPath(int root, int v, vector<int> parents);
     void CC();
     void print();
     float degMean();
+    float distMean();
     float relFrequency(int degree);
 };
- 
+
 // Utility function that, given parents vector, returns minimal path between root and v
 vector<int> Graph::MinPath(int root, int v, vector<int> parent) {
 	int i = v;
@@ -71,10 +76,7 @@ vector<int> Graph::MinPath(int root, int v, vector<int> parent) {
 
 //Implements Dijkstra's algorithm with a heap
 vector<bool> Graph::Dijkstra(int root) {
-
-
 	int i;
-
 	vector<float> dist;
 	vector<float> backup;
 	vector<int> parent(m_V,-1);
@@ -86,7 +88,7 @@ vector<bool> Graph::Dijkstra(int root) {
 		heap.insertKey(1e4);
 		dist.push_back(1e4);
 		backup.push_back(1e4);
-	}	
+	}
 
 	dist[root] = 0;
 	backup[root] = 0;
@@ -111,36 +113,34 @@ vector<bool> Graph::Dijkstra(int root) {
       			dist[pCrawl->dest] = dist[u] + pCrawl->weight;
       			backup[pCrawl->dest] = dist[pCrawl->dest];
       			parent[pCrawl->dest] = u;
-      		} 
+      		}
 	        pCrawl = pCrawl->next;
-	    }	
+	    }
 
 //	    heap.dump();
 //		cout << heap.getMin() << ' ';
-		backup[u] = -1;		
+		backup[u] = -1;
 		heap.extractMin();
 		i++;
 	}
 
-	
+
 	//for(int i=0;i<dist.size();i++)
-	cout << dist[0] << ' ';
-	cout << endl;
+	// cout << dist[0] << ' ';
+	// cout << endl;
 
 	minpath = MinPath(root,0,parent);
 
-	cout << "{";
-	for(int i=0;i<minpath.size();i++)
-		cout << minpath[i] << ',';
-	cout << "}" << endl;
-
-	cout << endl;
-
+	// cout << "{";
+	// for(int i=0;i<minpath.size();i++)
+	// 	cout << minpath[i] << ',';
+	// cout << "}" << endl;
+  //
+	// cout << endl;
+  this->dijkstraDists = dist;
 
 	std::vector<bool> v;
 	return v;
-
-
 }
 
 
@@ -155,11 +155,11 @@ Graph::Graph(int V, bool type) {
         for(int i=0;i<V;i++) {
             AdjList newList;
             newList.head = NULL;
-            m_adjArray.push_back(newList);        
-        }        
+            m_adjArray.push_back(newList);
+        }
     }
     else {
-        for(int i=0;i<V;i++) 
+        for(int i=0;i<V;i++)
             m_adjMatrix.resize(V);
     };
 }
@@ -180,7 +180,7 @@ AdjListNode* Graph::newNode (int dest, float weight) {
     return newNode;
 }
 
-void Graph::addEdge(int src, int dest) { 
+void Graph::addEdge(int src, int dest) {
 	// Unweighed edges
 
 	m_E++;
@@ -204,7 +204,7 @@ void Graph::addEdge(int src, int dest) {
 		m_adjMatrix[src][dest]=1;
 
 		// Since graph is undirected, add an edge from dest to src also
-		m_adjMatrix[dest][src]=1;	
+		m_adjMatrix[dest][src]=1;
 	}
 }
 
@@ -232,7 +232,7 @@ void Graph::addEdge(int src, int dest, float weight) {
 		m_adjMatrix[src][dest]=1;
 
 		// Since graph is undirected, add an edge from dest to src also
-		m_adjMatrix[dest][src]=1;	
+		m_adjMatrix[dest][src]=1;
 	}
 }
 
@@ -240,7 +240,7 @@ float Graph::degMean() {
      return float(2*m_E/m_V);
 }
 
-vector<int> Graph::BFS(int root) {
+float Graph::BFS(int root) {
 
 	clock_t begin = clock();
 
@@ -257,7 +257,7 @@ vector<int> Graph::BFS(int root) {
 	level[src] = 0;
 	queue.push(src);
 
-	cout << endl<< "BFS(" << root << ") Running..." << endl;
+	// cout << endl<< "BFS(" << root << ") Running..." << endl;
 
 	while(!queue.empty()) {
 		//Dequeue a vertex from queue and print it
@@ -267,8 +267,6 @@ vector<int> Graph::BFS(int root) {
 
 		if (m_type == 0) {
 			// Adjency list
-
-
 			AdjListNode* pCrawl = m_adjArray[s].head;
 	        while (pCrawl!=NULL) {
 	      		if(visited[pCrawl->dest] == 0) {
@@ -276,9 +274,9 @@ vector<int> Graph::BFS(int root) {
 	      			parent[pCrawl->dest] = s;
 	      			level[pCrawl->dest]=level[s]+1;
 	      			queue.push(pCrawl->dest);
-	      		} 
+	      		}
 		        pCrawl = pCrawl->next;
-		    }	
+		    }
 		} else {
 			//Adjency matrix
 			for(int i=0;i<m_V;i++) {
@@ -287,19 +285,32 @@ vector<int> Graph::BFS(int root) {
 					parent[i] = s;
 	      			level[i]=level[s]+1;
 					queue.push(i);
-				}	
+				}
 			}
 		}
 	}
 
+  //Distances mean calc
+  int dist[m_V];
+  float dist_sum = 0;
+  for (int i=0; i < m_V; i++){
+    int parentx = parent[i];
+    dist[i] = level[i];
+    while (level[parentx] != 0){
+      dist[i] += level[parentx];
+      parentx = parent[parentx];
+    }
+    dist_sum += dist[i];
+  }
+
 	//for (int i=9;i<50;i=i+10) cout << "Parent of " << i+1 << " = " << parent[i]+1 << "; " << "Level " << level[i] << endl;
 
-	clock_t	end = clock();
-	double elapsed_time = double(end-begin)/CLOCKS_PER_SEC;
 
-	//cout << "-------------------" << endl << "Elapsed time: " << elapsed_time << "s" << endl;
+  clock_t	end = clock();
+  double elapsed_time = double(end-begin)/CLOCKS_PER_SEC;
 
-	return explored;
+  //cout << "-------------------" << endl << "Elapsed time: " << elapsed_time << "s" << endl;
+  return dist_sum/m_V;
 }
 
 vector<bool> Graph::DFS(int root) {
@@ -335,9 +346,9 @@ vector<bool> Graph::DFS(int root) {
 	      			parent[pCrawl->dest] = s;
 	      			level[pCrawl->dest]=level[s]+1;
 	      			stack.push(pCrawl->dest);
-	      		} 
+	      		}
 		        pCrawl = pCrawl->next;
-		    }	
+		    }
 		} else {
 			//Adjency matrix
 			for(int i=0;i<m_V;i++) {
@@ -346,7 +357,7 @@ vector<bool> Graph::DFS(int root) {
 					parent[i] = s;
 	      			level[i]=level[s]+1;
 					stack.push(i);
-				}	
+				}
 			}
 		}
 	}
@@ -354,7 +365,7 @@ vector<bool> Graph::DFS(int root) {
 	//for (int i=9;i<50;i=i+10) cout << "Parent of " << i+1 << " = " << parent[i]+1 << "; " << "Level " << level[i] << endl;
 
 	clock_t	end = clock();
-	double elapsed_time = double(end-begin)/CLOCKS_PER_SEC;
+	//double elapsed_time = double(end-begin)/CLOCKS_PER_SEC;
 
 	//cout << "-------------------" << endl << "Elapsed time: " << elapsed_time << "s" << endl;
 
@@ -369,7 +380,7 @@ void Graph::CC () {
 	vector<bool>::iterator it;
 
 	it = find(explored.begin(),explored.end(),0);
-	
+
 	while(it!=explored.end()) {
 		vector<bool> visited = DFS(it-explored.begin()+1);
 		vector<int> buffer;
@@ -428,69 +439,158 @@ void Graph::print() {
     }
 }
 
+float Graph::distMean() {
+  for (int i=0; i < this->m_V; i++){
+    this->Dijkstra(i);
+  }
+}
+//
+// struct DisjointSets{
+//     int *parent, *rnk;
+//     int n;
+//
+//     DisjointSets(int n){
+//         this->n = n;
+//         parent = new int[n+1];
+//         rnk = new int[n+1];
+//
+//         for (int i = 0; i <= n; i++){
+//             rnk[i] = 0;
+//             parent[i] = i;
+//         }
+//     }
+//
+//     int find(int u){
+//         if (u != parent[u])
+//             parent[u] = find(parent[u]);
+//         return parent[u];
+//     }
+//
+//     void merge(int x, int y){
+//         x = find(x), y = find(y);
+//
+//         if (rnk[x] > rnk[y])
+//             parent[y] = x;
+//         else
+//             parent[x] = y;
+//
+//         if (rnk[x] == rnk[y])
+//             rnk[y]++;
+//     }
+// };
+//
+// int Graph::Kruskal(){
+//     int k_weight = 0;
+//
+//     for (int i=0; i<m_V; i++)
+//       sort(m_adjArray.begin(), m_adjArray.end());
+//
+//     DisjointSets ds(m_V);
+//
+//     iterator it;
+//
+//     // ofstream filex;
+//     // filex.open("nmst_rede_colaboracao.txt");
+//     // filex << "Arestas da MST (origem-destino [peso]):\n";
+//
+//     for (it=m_adjArray.begin(); it!=m_adjArray.end(); it++){
+//         int u = it->second.first;
+//         int v = it->second.second;
+//
+//         int set_u = ds.find(u);
+//         int set_v = ds.find(v);
+//
+//         if (set_u != set_v){
+//             k_weight += it->first;
+//             ds.merge(set_u, set_v);
+//         }
+//     }
+//     return k_weight;
+// }
+
 // Driver program to test above functions
 int main(int argc, char** argv) {
 
     ifstream data;
-    data.open("grafo_4.txt");
+    data.open("grafo_2.txt");
 
     ofstream emp_dist;
-    emp_dist.open("dblp_dist.txt");
+    emp_dist.open("teste.txt");
 
     bool type = 0;
     //cout << "Define data structure type: 0-Adjency list; 1-Adjency Matrix" << endl;
     //cin >> type;
+    clock_t begin = clock();
 
     // Read V from file and create new graph with V vertexes and 0 edges
     int V;
+    float mean = 0;
+    float m;
     data >> V;
-    Graph graph(V, type);
+    for (int i=0; i < V; i++){
+      Graph graph(V, type);
 
-    //Dynamically create edges
-    int v1,v2;
-    float w;
-    while (data >> v1 >> v2 >> w) {
-    	if(w<0) {
-			cout << "Negative weight detected!" << endl;
-			return 0;
-		}
-        graph.addEdge(v1-1,v2-1, w);
+      //Dynamically create edges
+      int v1,v2;
+      float w;
+      while (data >> v1 >> v2 >> w) {
+      	if(w<0) {
+  			     cout << "Negative weight detected!" << endl;
+  			return 0;
+      } else if (v1-1 >= i && v2-1 >= i){
+          graph.addEdge(v1-1,v2-1, w);
+        }
+      }
+      graph.Dijkstra(i);
+      m = 0;
+      for (int j=0; j < V-i; j++){
+        m += graph.dijkstraDists[j];
+      }
+      mean += m/V;
+      data.close();
+      data.open("grafo_2.txt");
+      data >> V;
+
     }
+    mean = m/V;
+    cout << mean << endl;
 
     data.close();
     //cout << "edges added" << endl;
     //pause();
 
     //Print data
-    cout << "Number of vertexes: " <<  graph.m_V << endl;
-    cout << "Number of edges: " << graph.m_E << endl;
-    cout << "Degree mean: " << graph.degMean() << endl;
+    // cout << "Number of vertexes: " <<  graph.m_V << endl;
+    // cout << "Number of edges: " << graph.m_E << endl;
+    // cout << "Degree mean: " << graph.degMean() << endl;
 
-    double max = 0;
-    double min = 999999999999;
+    // double max = 0;
+    // double min = 999999999999;
+    //
+    // for (int i=0;i<graph.m_V/1000;i++) {
+		// emp_dist << i << " " << graph.relFrequency(i) << endl;
+		// if (max < graph.relFrequency(i)) max = graph.relFrequency(i);
+		// if (min > graph.relFrequency(i)) min = graph.relFrequency(i);
+    // }
+    //
+    // emp_dist.close();
 
-    for (int i=0;i<graph.m_V/1000;i++) {
-		emp_dist << i << " " << graph.relFrequency(i) << endl;
-		if (max < graph.relFrequency(i)) max = graph.relFrequency(i);    	
-		if (min > graph.relFrequency(i)) min = graph.relFrequency(i);    	
-    } 
+    // cout << "max: " << max << endl;
+    // cout << "min: " << min << endl;
+    //  graph.print();
 
-    emp_dist.close();
 
-    cout << "max: " << max << endl;
-    cout << "min: " << min << endl;
-//    graph.print();
-	clock_t begin = clock();
+    // First Study Case
+  	// graph.Dijkstra(9);
+  	// graph.Dijkstra(19);
+  	// graph.Dijkstra(29);
+  	// graph.Dijkstra(39);
+  	// graph.Dijkstra(49);
 
-	graph.Dijkstra(10);
-	graph.Dijkstra(20);
-	graph.Dijkstra(30);
-	graph.Dijkstra(40);
-	graph.Dijkstra(50);
 
-	clock_t	end = clock();
-	double elapsed_time = double(end-begin)/CLOCKS_PER_SEC;
-	cout << "-------------------" << endl << "Dijsktra elapsed time: " << elapsed_time << "s" << endl;
+  	clock_t	end = clock();
+  	double elapsed_time = double(end-begin)/CLOCKS_PER_SEC;
+  	cout << "-------------------" << endl << "Elapsed time: " << elapsed_time << "s" << endl;
 
 
     //graph.DFS(1);
